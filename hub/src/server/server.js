86f7a5c17695a6ed050de3b92b29263809df4991
Aaron Blankstein <aaron@blockstack.com>
2018-05-01 18:12:06 -0400

    initial work on inboxes

--- hub/src/server/server.js
+++ hub/src/server/server.js
@@ -1,8 +1,40 @@
+/* @flow */
+
 import { StorageAuthentication as StorageAuth } from './StorageAuthentication'
 import { ValidationError } from './errors'
 import logger from 'winston'
+import stream from 'stream'
+
+export type REQUEST_HEADERS = { authorization: string,
+                                'content-type': string,
+                                'content-length': string }
+
+function bufferStream(input: stream.Readable, maxLength: number) {
+  return new Promise((resolve, reject) => {
+    let buffers = []
+    let length = 0
+    input.on('error', reject)
+    input.on('data', (data) => {
+      if (length < maxLength) {
+        buffers.push(data)
+        length += data.length
+      } else {
+        if (data.length > 0) {
+          reject(new ValidationError('Too much data uploaded'))
+        }
+      }
+    })
+    input.on('end', () => resolve(
+      Buffer.concat(buffers)
+        .slice(0, maxLength)))
+  })
+}
 
 export class HubServer {
+  driver: Object
+  proofChecker: Object
+  whitelist: Array<string>
+  serverName: string
   constructor(driver: Object, proofChecker: Object,
               config: { whitelist: Array<string>, servername: string }) {
     this.driver = driver
@@ -33,7 +65,7 @@ export class HubServer {
   }
 
   handleRequest(address: string, path: string,
-                requestHeaders: {},
+                requestHeaders: REQUEST_HEADERS,
                 stream: stream.Readable) {
     this.validate(address, requestHeaders)
 
@@ -49,5 +81,22 @@ export class HubServer {
 
     return this.proofChecker.checkProofs(address, path)
       .then(() => this.driver.performWrite(writeCommand))
+  }
+
+  putInboxMessage(destinationAddress: string,
+                  senderAddress: string,
+                  requestHeaders: REQUEST_HEADERS,
+                  stream: stream.Readable) {
+    this.validate(senderAddress, requestHeaders)
+    const contentLength = parseInt(requestHeaders['content-length']) > this.inboxItemSize ?
+          this.inboxItemSize : parseInt(requestHeaders['content-length'])
+
+    return bufferStream(stream)
+      .then( messageBuffer => {
+        return this.driver.getReadURLPrefix()
+          .then(readUrlPrefix => {
+            const inboxUrl = `${readUrlPrefix}/${destinationAddress}-inbox/`
+          })
+      })
   }
 }
